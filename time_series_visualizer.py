2aecdf1c3dfad660bff8de5d77ad95d73fa97609
Pickledtezcat <johnmtopple@gmail.com>
2022-05-10 07:03:48 +0000

    Attempting to commit

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -4,6 +4,8 @@ import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
+
+print("testing")
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
 df = None
 
@@ -23,32 +25,36 @@ def draw_line_plot():
     return fig
 
 def draw_bar_plot():
+
+  return None
     # Copy and modify data for monthly bar plot
-    df_bar = None
+  df_bar = None
 
-    # Draw bar plot
-
-
+  # Draw bar plot
 
 
 
-    # Save image and return fig (don't change this part)
-    fig.savefig('bar_plot.png')
-    return fig
+
+
+  # Save image and return fig (don't change this part)
+  fig.savefig('bar_plot.png')
+  return fig
 
 def draw_box_plot():
-    # Prepare data for box plots (this part is done!)
-    df_box = df.copy()
-    df_box.reset_index(inplace=True)
-    df_box['year'] = [d.year for d in df_box.date]
-    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+  return None
+  
+  # Prepare data for box plots (this part is done!)
+  df_box = df.copy()
+  df_box.reset_index(inplace=True)
+  df_box['year'] = [d.year for d in df_box.date]
+  df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
-    # Draw box plots (using Seaborn)
+  # Draw box plots (using Seaborn)
 
 
 
 
 
-    # Save image and return fig (don't change this part)
-    fig.savefig('box_plot.png')
-    return fig
+  # Save image and return fig (don't change this part)
+  fig.savefig('box_plot.png')
+  return fig
